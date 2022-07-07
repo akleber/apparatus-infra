37136bd4bb598c148c904a4b6c43edae300e5e77
Andreas Kleber <andreas@drosselweg7a.de>
2022-07-07 20:37:56 +0200

    ensure python requirements are up tp date

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@
 cd /opt/apparatus
 runuser apparatus -c 'git pull --rebase'
 runuser apparatus -c 'git rev-parse --short HEAD > VERSION'
+runuser apparatus -c 'source venv/bin/activate && pip install -r requirements.txt'
 systemctl daemon-reload
 systemctl restart apparatus
