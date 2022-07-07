89608f253294a0a89318a4eb98116ff79a40c1bf
Andreas Kleber <andreas@drosselweg7a.de>
2022-07-07 20:35:17 +0200

    no loginshell for runuser

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 cd /opt/apparatus
-runuser -l apparatus -c 'git pull --rebase'
-runuser -l apparatus -c 'git rev-parse --short HEAD > VERSION'
+runuser apparatus -c 'git pull --rebase'
+runuser apparatus -c 'git rev-parse --short HEAD > VERSION'
 systemctl daemon-reload
 systemctl restart apparatus
